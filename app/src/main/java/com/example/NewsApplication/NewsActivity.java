99ca918c4c137c1237f9cdff87a95a433e7609d4
jinoochinoo <73393147+jinoochinoo@users.noreply.github.com>
2021-02-09 21:39:49 +0900

    JSON 타입으로 news data 세팅

--- app/src/main/java/com/example/NewsApplication/NewsActivity.java
+++ app/src/main/java/com/example/NewsApplication/NewsActivity.java
@@ -2,15 +2,33 @@ package com.example.NewsApplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NewsActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
-    private String[] mDataset;
+    private List<NewsData> mDataset;
+
+    RequestQueue queue;
 
     protected void onCreate(Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
@@ -25,10 +43,62 @@ public class NewsActivity extends AppCompatActivity {
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-        // adapter 세팅
-        mAdapter = new MyAdapter(mDataset);
-        mRecyclerView.setAdapter(mAdapter);
+        // Instantiate the RequestQueue // RequestQueue 초기화
+        queue = Volley.newRequestQueue(this);
+        getNews();
+    }
 
+    public void getNews(){
+
+        String url ="https://newsapi.org/v2/everything?q=bitcoin&apiKey=d6b5d0172d0346278db2d6500b65a79d";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        Log.d("news", response);
+
+                        try{
+                            JSONObject jsonObj = new JSONObject(response);
+
+                            JSONArray arrayArticles = jsonObj.getJSONArray("articles");
+
+                            // NewsData 선언 및 생성 -> 분류
+                            List<NewsData> news = new ArrayList<>();
+
+                            // 반복문 통해 news 데이터 JSON 형태로 주입
+                            for(int i=0, j=arrayArticles.length(); i<j; i++){
+                                JSONObject obj = arrayArticles.getJSONObject(i);
+
+                                Log.d("news", obj.toString());
+
+                                NewsData newsData = new NewsData();
+                                newsData.setTitle(obj.getString("title"));
+                                newsData.setUrlToImage(obj.getString("urlToImage"));
+                                newsData.setContent(obj.getString("content"));
+                                news.add(newsData);
+                            }
+
+                            // adapter 세팅
+                            mAdapter = new MyAdapter(mDataset);
+                            mRecyclerView.setAdapter(mAdapter);
+
+                        } catch(JSONException e){
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 
 }
---
+++ app/src/main/java/com/example/NewsApplication/NewsData.java
@@ -0,0 +1,34 @@
+package com.example.NewsApplication;
+
+import java.io.Serializable;
+
+public class NewsData implements Serializable {
+
+    private String title;
+    private String urlToImage;
+    private String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getUrlToImage() {
+        return urlToImage;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setUrlToImage(String urlToImage) {
+        this.urlToImage = urlToImage;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
